f28d3c668def14caee1a6ab294d7a60c106679ad
Javier Lopez Gozalez <100405801@alumnos.uc3m.es>
2022-11-07 10:35:24 +0100

    Gitignore

--- MeadowTW1/Secrets.cs
+++ MeadowTW1/Secrets.cs
@@ -3,8 +3,8 @@
 
         //WiFi NAME 
         //public const string WIFI_NAME = "vodafoneF530";
-        public const string WIFI_NAME = "MiFibra-434F";
-        //public const string WIFI_NAME = "Oppo Reno8";
+        //public const string WIFI_NAME = "MiFibra-434F";
+        public const string WIFI_NAME = "Oppo Reno8";
 
 
         //PASSWORD FOR THE WiFi NAME
