aac172f1329c291682490524a425d9975413c492
Javier Lopez Gozalez <100405801@alumnos.uc3m.es>
2022-11-12 21:56:02 +0100

    Codigo sin mierdas. Estoy hasta la polla del git

---
+++ MeadowTW1/Secrets.cs
@@ -0,0 +1,14 @@
+﻿namespace NewCode {
+    internal class Secrets {
+
+        //WiFi NAME 
+        //public const string WIFI_NAME = "vodafoneF530";
+        //public const string WIFI_NAME = "MiFibra-434F";
+        public const string WIFI_NAME = "Oppo Reno8";
+
+
+        //PASSWORD FOR THE WiFi NAME
+        //public const string WIFI_PASSWORD = "JJY4YWN2YGZ3MT";
+        public const string WIFI_PASSWORD = "uCcNe9gP";
+    }
+}
